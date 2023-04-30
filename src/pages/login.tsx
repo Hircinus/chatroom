439008d590d7a05b881b5e4525dba721d8ce599c
Émilie Parent <102536784+Sprytte@users.noreply.github.com>
2023-04-30 00:09:34 -0400

    added the pages

---
+++ src/pages/login.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <h1 className={`text-xl`}>
+        Login
+      </h1>
+      <div className={`text-center p-3 bg-blue-300`}>
+        <input type="text"></input>
+      </div>
+    </main>
+  )
+}
